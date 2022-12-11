a83a655ec1d811ad572b5abcaf9af1dece953bdd
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2022-12-11 10:59:50 +0530

    Js imported

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello vanillaJS");
+console.log("Hello! script is working");
+//input
+var name = prompt("What is your name: ");
+
+//Processing
+var welcomeMessage = prompt("This script works " + name );
+
+
+//Output
+alert(welcomeMessage);
+
