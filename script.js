58d076ea1fbcf81552a3ee426af40abd478666ab
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2022-12-11 11:05:27 +0530

    Button added "Click me"

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-console.log("Hello! script is working");
-//input
-var name = prompt("What is your name: ");
 
-//Processing
-var welcomeMessage = prompt("This script works " + name );
-
-
-//Output
-alert(welcomeMessage);
 
